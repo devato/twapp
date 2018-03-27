4ade5971b942e036ef420b0d5bbac9d59ff16364
tmartin314 <troy@devatotech.com>
2018-03-27 04:16:57 -0400

    comments

--- docker/entrypoints/api.sh
+++ docker/entrypoints/api.sh
@@ -10,8 +10,8 @@ until curl $DB_HOST:5432 2>&1 | grep '52'; do
 done
 echo "PostgreSQL is up and running"
 
-bundle check || bundle install --binstubs="$BUNDLE_BIN"
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
+bundle check || bundle install --binstubs="$BUNDLE_BIN"
 
 # Setup db and/or run migrations
 bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:create db:migrate db:seed
